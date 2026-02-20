cfc48637affa92e6d43eb58c491472ec8454858c
Los Kirill <kirilosar@gmail.com>
2026-02-20 22:37:33 +0400

    в программу добавленно чтение из файла.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,24 +1,61 @@
 package org.example;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        File file = new File("Calculator.txt");
+        File file = new File("Input.txt");
 
         try {
             file.createNewFile();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Введите пример формата (Число пробел операция пробел число)");
-        String num1 = sc.next();
-        String oper = sc.next();
-        String num2 = sc.next();
+        String filenumber = "";
+        String num1 = "", oper = "", num2 = "";
+        StringBuffer stringBuffer = new StringBuffer();
+        int k = 0;
+        try (FileInputStream fis = new FileInputStream(file)){
+            int a = fis.read();
+            while (a != -1){
+                if (a == ' '){
+                    if(k == 0) {
+                        num1 = stringBuffer.toString();
+                        stringBuffer.setLength(0);
+                        k++;
+                    }else if(k == 1){
+                        oper = stringBuffer.toString();
+                        stringBuffer.setLength(0);
+                        k++;
+                    }
+                }else{
+                    stringBuffer.append((char) a);
+                }
+//                else {
+//                    stringBuffer.append((char) a);
+//                    num2 = stringBuffer.toString();
+//                    stringBuffer.setLength(0);
+//                    k++;
+//                }
+                a = fis.read();
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            num2 = stringBuffer.toString();
+            stringBuffer.setLength(0);
+        }
+        System.out.println(filenumber);
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Введите пример формата (Число пробел операция пробел число)");
+//        num1 = sc.next();
+//        oper = sc.next();
+//        num2 = sc.next();
         String str = "";
         try {
             double numb1 = Double.parseDouble(num1);
@@ -38,11 +75,11 @@ public class Main {
             str = e.getMessage();
         }
         System.out.println(str.equals("Infinity") ? "Error! Division by zero" : str);
-        try (FileOutputStream fos = new FileOutputStream(file)) {
-            fos.write(str.getBytes());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+//        try (FileOutputStream fos = new FileOutputStream(file)) {
+//            fos.write(str.getBytes());
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
 
     }
 }
